ff9367df96bec482618c68fead4ae60c0ca7029f
Rudolf Kolbe <rkolbe96@gmail.com>
2026-01-19 12:37:32 +0100

    fix build

--- compressonator_pyc/core_simd_stub.cpp
+++ compressonator_pyc/core_simd_stub.cpp
@@ -6,14 +6,31 @@ so we have to provides stubs for archs that don't support them
 #include "core_simd.h"
 
 float sse_bc1ComputeBestEndpoints(float *, float *, float *, float *, float *,
-                                  int, int) {
+                                  int, int)
+{
   throw "Not implemented";
 }
 float avx_bc1ComputeBestEndpoints(float *, float *, float *, float *, float *,
-                                  int, int) {
+                                  int, int)
+{
   throw "Not implemented";
 }
 float avx512_bc1ComputeBestEndpoints(float *, float *, float *, float *,
-                                     float *, int, int) {
+                                     float *, int, int)
+{
   throw "Not implemented";
 }
+
+#ifdef _WIN32
+#include <intrin.h>
+#ifndef __cpuidex
+extern "C"
+{
+  inline void __cpuidex(int CPUInfo[4], int function_id, int subfunction_id)
+  {
+    (void)function_id;
+    (void)subfunction_id;
+    CPUInfo[0] = CPUInfo[1] = CPUInfo[2] = CPUInfo[3] = 0;
+  }
+}
+#endif
